31653480a731b13f2dd0e878a76e9d34409619ee
Yogeeswaran03 <yogees97@gmail.com>
2025-11-18 19:58:06 +0530

    Quick changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -28,6 +29,7 @@ function AppRoutes() {
 
   return (
     <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -68,6 +70,7 @@ export default function App() {
         </AuthProvider>
 
       </TooltipProvider>
+      <Analytics />
     </QueryClientProvider>
   );
 }
